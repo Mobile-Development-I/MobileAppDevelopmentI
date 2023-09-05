105ac448d1dd1ed2e82d8a02c2edbd5f384413fb
Jaresi Nunez <58448481+jaresinunez@users.noreply.github.com>
2023-09-05 16:45:43 -0500

    first commit

---
+++ HelloWorld/app/src/main/java/com/jaresi/helloworld/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jaresi.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Log.v("Say Hello button", "button clicked!")
+        }
+
+    }
+}
